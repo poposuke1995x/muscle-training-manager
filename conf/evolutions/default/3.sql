cec722de94f4bebf02c9e6c126683ed2c0bbd3e0
poposuke1995x <jimi.woodstock1995@gmail.com>
2020-10-02 03:17:33 +0900

    マイグレーションファイルの修正

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -2,9 +2,11 @@
 
 -- !Ups
 
+create type category as enum('胸', '背中', '肩', '脚', '腹筋・体幹', '有酸素', 'その他');
+
 create table if not exists categories (
   id serial primary key,
-  name varchar(50) not null
+  name category not null
   );
 
 insert into categories (name) values ('胸');
