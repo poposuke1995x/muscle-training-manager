f5dee95fdb08973270f16a63c3eb280a4900e3c5
poposuke1995x <jimi.woodstock1995@gmail.com>
2020-10-04 18:17:00 +0900

    種目一覧

---
+++ app/controllers/GetLiftTypeController.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import com.google.inject.Inject
+import org.json4s.DefaultFormats
+import org.json4s.native.Serialization
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+import usecase.GetLiftTypeService
+import scala.concurrent.ExecutionContext
+
+class GetLiftTypeController @Inject()
+(controllerComponents: ControllerComponents, getLiftTypeService: GetLiftTypeService)
+(implicit executionContext: ExecutionContext) extends AbstractController(controllerComponents) {
+
+  implicit val formats: DefaultFormats.type = DefaultFormats
+
+  def show(liftTypeId: Int): Action[AnyContent] = Action.async {
+    getLiftTypeService(liftTypeId).map(liftType =>
+      Ok(Serialization.write(liftType))
+    )
+  }
+}
