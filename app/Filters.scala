601aa7b132c8a11124d978edf3c687287b482ba7
poposuke1995x <jimi.woodstock1995@gmail.com>
2020-11-01 06:28:39 +0900

    application.confのフォーマットを修正/cors回避

--- app/Filters.scala
+++ app/Filters.scala
@@ -1,7 +1,11 @@
 import com.google.inject.Inject
 import play.api.http.HttpFilters
 import filters.AuthFilter
+import play.filters.cors.CORSFilter
 
-class Filters @Inject()(authFilter: AuthFilter) extends HttpFilters {
-  val filters = Seq()
+class Filters @Inject()(authFilter: AuthFilter, corsFilter: CORSFilter) extends HttpFilters {
+  val filters = Seq(
+    authFilter,
+    corsFilter
+  )
 }
