22a5b83ab5e74e081b897cfa8a98b5bd8fce6f6f
poposuke1995x <jimi.woodstock1995@gmail.com>
2020-11-01 19:14:45 +0900

    dbの設定変更

--- docker/workspace/config.fish
+++ docker/workspace/config.fish
@@ -10,4 +10,15 @@ set -U FZF_LEGACY_KEYBINDINGS 0
 set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
 #################################################
 
+
+################# 自動でls ######################
+functions --copy cd standard_cd
+function cd
+    standard_cd $argv; and ls
+ end
+#################################################
+
 alias sbt="/root/.sdkman/candidates/sbt/current/bin/sbt"
+
+# ローカル開発環境のDB設定
+set -x JDBC_DATABASE_URL "jdbc:postgresql://postgres:5432/postgres?user=postgres&password=postgres"
