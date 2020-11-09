9f29afae77f8c22c417c341c547d16de473e42f6
poposuke1995x <jimi.woodstock1995@gmail.com>
2020-11-10 00:48:08 +0900

    ブロッキングな処理をなくす

--- app/filters/AuthFilter.scala
+++ app/filters/AuthFilter.scala
@@ -9,50 +9,53 @@ import play.api.Configuration
 import play.api.mvc.Results.Forbidden
 import play.api.mvc.{Filter, RequestHeader, Result}
 import utils.Utils
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.util.{Failure, Success}
+import scala.concurrent.{ExecutionContext, Future}
 
-class AuthFilter @Inject()(config: Configuration, utils: Utils)(implicit executionContext: ExecutionContext, implicit val mat: Materializer) extends Filter {
-  val serviceAccount = new FileInputStream(config.get[String]("secret.key.path"))
+class AuthFilter @Inject()(config: Configuration, utils: Utils)
+    (
+        implicit executionContext: ExecutionContext,
+        implicit val mat: Materializer
+    ) extends Filter {
+
+  val serviceAccount = new FileInputStream(
+    config.get[String]("secret.key.path")
+  )
   val options: FirebaseOptions =
     new FirebaseOptions.Builder()
-      .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-      .build
+        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        .build
   FirebaseApp.getApps.size match {
     case 0 => FirebaseApp.initializeApp(options)
     case _ => FirebaseApp.getApps.get(0)
   }
 
-
-  def apply(nextFilter: RequestHeader => Future[Result])(rh: RequestHeader): Future[Result] = {
+  def apply(
+      nextFilter: RequestHeader => Future[Result]
+  )(rh: RequestHeader): Future[Result] = {
     implicit def Pipeline[T](x: T): utils.Pipeline[T] = utils.Pipeline(x)
 
-    rh.headers.get("Authorization").getOrElse("") |> utils.getFirebaseUid
-    match {
-      case uid if uid.nonEmpty => nextFilter(rh).map(_.withHeaders {
-        addUserIdToHeader(uid)
-      })
+    rh.headers
+        .get("Authorization")
+        .getOrElse("") |> utils.getFirebaseUid match {
+      case uid if uid.nonEmpty =>
+        addUserIdToHeader(uid) flatMap { userIdHeader =>
+          nextFilter(rh) map {
+            _.withHeaders(userIdHeader)
+          }
+        }
       case _ => Future(Forbidden("invalid"))
     }
   }
 
-
-  def addUserIdToHeader(uid: String): (String, String) = {
+  def addUserIdToHeader(uid: String): Future[(String, String)] = {
     implicit def Pipeline[T](x: T): utils.Pipeline[T] = utils.Pipeline(x)
 
-    val userIdHeader = utils.getUserId(uid).flatMap { userId =>
+    utils.getUserId(uid) flatMap { userId =>
       if (userId != 0) Future(("user_id", userId.toString))
-      else (uid |> utils.createGuestUser).map { userId => ("user_id", userId.toString) }
-    }
-
-    Await.result(userIdHeader, Duration.Inf)
-
-    userIdHeader.value.get match {
-      case Success(userIdHeader) => userIdHeader
-      case Failure(e) =>
-        print(e)
-        ("user_id", "")
+      else
+        (uid |> utils.createGuestUser) map { userId =>
+          ("user_id", userId.toString)
+        }
     }
   }
 }
