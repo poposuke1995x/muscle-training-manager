1f214bea743f9e5425c511c6687b94737dec9596
poposuke1995x <jimi.woodstock1995@gmail.com>
2020-12-13 22:19:33 +0900

    パスワードの更新を実装

--- app/dto/Requests.scala
+++ app/dto/Requests.scala
@@ -1,5 +1,7 @@
 package dto
 
+import scala.util.matching.Regex
+
 case class TargetedLiftTypeRequest(
     id: Int,
     lightRep: Int = 0,
@@ -41,3 +43,14 @@ case class MenuLiftActionRequest(
     userId: Int = 0,
     name: Option[String]
 )
+
+case class UpdatePasswordRequest(newPassword: String, confirmPassword: String) {
+  def validate: Either[String, String] = {
+    val matchPattern: Regex = "^(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9!\"#$%&'()\\-^@;:,./|`{+*}<>?_]{8,32}$".r
+    newPassword match {
+      case newPassword if newPassword != confirmPassword => Left("パスワードと確認用パスワードが不一致です")
+      case matchPattern() => Right(newPassword)
+      case _ => Left("パスワードは8~32文字の半角英数字で指定してください")
+    }
+  }
+}
